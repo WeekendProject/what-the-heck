13f84a3c0fff469fa5c6304fab84fdeb4139685b
Alex Lementuev <a.lementuev@gmail.com>
2016-06-11 15:47:20 -0700

    Cat head progress

---
+++ Assets/Scripts/CatHead.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatHead : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("A transform to follow")]
+    Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
