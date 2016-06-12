172570392cefe41bc58e8ef518fe3ee2339b9b05
Alex Lementuev <a.lementuev@gmail.com>
2016-06-11 23:10:35 -0700

    Laser beam progress

---
+++ Assets/Objects/Laser Beam/Scripts/LaserBeamAnimator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using VolumetricLines;
+
+[RequireComponent(typeof(VolumetricLineBehavior))]
+public class LaserBeamAnimator : MonoBehaviour
+{
+    [SerializeField]
+    float m_minFactor = 0.85f;
+
+    [SerializeField]
+    float m_maxFactor = 0.95f;
+
+    [SerializeField]
+    float m_speed = 10.0f;
+
+    VolumetricLineBehavior m_line;
+    float m_totalTime;
+
+	void Awake ()
+    {
+        m_line = GetComponent<VolumetricLineBehavior>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_line.LineWidth = Mathf.Lerp(m_minFactor, m_maxFactor, Mathf.Sin(m_speed * m_totalTime));
+        m_totalTime += Time.deltaTime;   
+	}
+}
