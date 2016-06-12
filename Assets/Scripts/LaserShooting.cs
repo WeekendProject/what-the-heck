03073e55c2d2bb3f6ef7a3561dbf338cfa6a4369
Alex Lementuev <a.lementuev@gmail.com>
2016-06-11 21:29:56 -0700

    Laser shooting progress

---
+++ Assets/Scripts/LaserShooting.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShooting : MonoBehaviour {
+
+    [SerializeField]
+    Transform m_leftEye;
+
+    [SerializeField]
+    Transform m_rightEye;
+
+    public void ShootLasers()
+    {
+
+    }
+}
