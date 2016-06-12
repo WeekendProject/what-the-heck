c858efc288809bef6df16d34adab340d959d8f84
Alex Lementuev <a.lementuev@gmail.com>
2016-06-11 21:17:38 -0700

    Added random head movement

---
+++ Assets/Scripts/FloatingMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingMovement : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 m_velocity = new Vector3(0.1f, 0.1f, 0.0f);
+
+    [SerializeField]
+    Vector3 m_scale = new Vector3(1, 1, 1);
+
+    float m_elaspedTime;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        float vx = Mathf.Sin(m_scale.x * m_elaspedTime) * Mathf.Cos(m_scale.x * m_elaspedTime) * m_velocity.x;
+        float vy = Mathf.Sin(m_scale.y * m_elaspedTime) * Mathf.Cos(m_scale.y * m_elaspedTime) * m_velocity.y;
+        float vz = Mathf.Sin(m_scale.z * m_elaspedTime) * Mathf.Cos(m_scale.z * m_elaspedTime) * m_velocity.z;
+
+        Vector3 offset = new Vector3(vx, vy, vz) * Time.deltaTime;
+        transform.position += offset;
+
+        m_elaspedTime += Time.deltaTime;
+	}
+}
