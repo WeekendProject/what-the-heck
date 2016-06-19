925b9278dabfbdc231dba5d991c1e1b5838c60db
Alex Lementuev <a.lementuev@gmail.com>
2016-06-19 14:23:14 -0700

    Explosions progress

---
+++ Assets/Editor/Commands.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+
+using LunarPlugin;
+
+[CCommand("explode")]
+class Cmd_explode : CCommand
+{
+    void Execute()
+    {
+        GameObject[] selection = Selection.gameObjects;
+        foreach (GameObject obj in selection)
+        {
+            Explodable explodable = obj.GetComponent<Explodable>();
+            if (explodable != null)
+            {
+                explodable.Explode();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Explodable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explodable : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] m_explosions;
+
+    [SerializeField]
+    GameObject m_exploded;
+
+    #region Explosions
+
+    public void Explode()
+    {
+        CreateExplosion();
+        CreateExploded();
+
+        Destroy(gameObject);
+    }
+    
+    void CreateExplosion()
+    {
+        int index = Random.Range(0, m_explosions.Length - 1);
+        Instantiate(m_explosions[index], transform.position, Quaternion.identity);
+    }
+
+    void CreateExploded()
+    {
+        if (m_exploded)
+        {
+            Instantiate(m_exploded, transform.position, Quaternion.identity);
+        }
+    }
+
+    #endregion
+}
