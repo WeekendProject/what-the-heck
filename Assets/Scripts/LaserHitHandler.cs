ff59a6e1db773d8849015e7af695439bb26028de
Alex Lementuev <a.lementuev@gmail.com>
2016-06-22 21:29:32 -0700

    Progress

--- Assets/Scripts/LaserHitHandler.cs
+++ Assets/Scripts/LaserHitHandler.cs
@@ -1,16 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using Valve.VR;
+
 [RequireComponent(typeof(SteamVR_LaserPointer))]
 public class LaserHitHandler : MonoBehaviour
 {
     SteamVR_LaserPointer m_laserPointer;
+    int m_controllerIndex;
+    Explodable m_explodable;
 
     void Awake()
     {
         m_laserPointer = GetComponent<SteamVR_LaserPointer>();
         m_laserPointer.PointerIn += OnPointerIn;
         m_laserPointer.PointerOut += OnPointerOut;
+        m_controllerIndex = -1;
+    }
+
+    void Update()
+    {
+        if (m_controllerIndex != -1 && m_explodable != null && SteamVR_Controller.Input(m_controllerIndex).GetPressDown(EVRButtonId.k_EButton_SteamVR_Trigger))
+        {
+            CatHead catHead = FindObjectOfType<CatHead>();
+            catHead.Attack(m_explodable);
+            m_explodable = null;
+        }
     }
 
     void Destroy()
@@ -23,12 +38,17 @@ public class LaserHitHandler : MonoBehaviour
 
     void OnPointerIn(object sender, PointerEventArgs e)
     {
+        m_controllerIndex = (int) e.controllerIndex;
+        print(m_controllerIndex);
+
         TrySetHighlighted(e.target.gameObject, true);
     }
 
     void OnPointerOut(object sender, PointerEventArgs e)
-    {
+    {   
         TrySetHighlighted(e.target.gameObject, false);
+        m_controllerIndex = -1;
+        m_explodable = null;
     }
 
     void TrySetHighlighted(GameObject hitObject, bool highlighted)
@@ -37,6 +57,11 @@ public class LaserHitHandler : MonoBehaviour
         if (selectable != null)
         {
             selectable.highlighted = highlighted;
+            m_explodable = selectable.GetComponent<Explodable>();
+        }
+        else
+        {
+            m_explodable = null;
         }
     }
 
